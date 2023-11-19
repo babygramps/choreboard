7e522dc342c000281a9523facd6989b02634c1f7
Richard Rothbart <richard.a.rothbart@gmail.com>
2023-11-18 18:39:44 -0800

    Added points and housemate removal logic

--- src/App.js
+++ src/App.js
@@ -1,95 +1,210 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import HousemateForm from './AddHousemateForm';
 import HousemateList from './HousemateList';
 import AddChoreForm from './AddChoreForm';
-
-import './ChoreLeaderboard.css'
+import './ChoreLeaderboard.css'; // Import the CSS file
 
 const App = () => {
-  const [housemates, setHousemates] = useState([]);
-  const [chores, setChores] = useState(["Bathrooms", "Commons", "Kitchen", "Vacation"]);
+    const [housemates, setHousemates] = useState([
+        {
+            name: "Rick",
+            chores: [],
+            points: 0
+        }, {
+            name: "Matt",
+            chores: [],
+            points: 0
+        }, {
+            name: "Thora",
+            chores: [],
+            points: 0
+        }, {
+            name: "Susan",
+            chores: [],
+            points: 0
+        }
+    ]);
+    const [chores, setChores] = useState(["Bathrooms", "Commons", "Kitchen", "Vacation"]);
 
-  // Function to add a new housemate
-  const addHousemate = (name) => {
-    const newHousemate = { name, chores: [] };
-    setHousemates(prevHousemates => [...prevHousemates, newHousemate]);
-  };
+    // Function to add a new housemate
+    const addHousemate = (name) => {
+        const newHousemate = {
+            name,
+            chores: [],
+            points: 0
+        };
+        setHousemates(prevHousemates => [
+            ...prevHousemates,
+            newHousemate
+        ]);
+    };
 
-  // Function to add a new chore to the list
-  const addChoreToList = (choreName) => {
-    if (!chores.includes(choreName)) {
-      setChores(prevChores => [...prevChores, choreName]);
-    } else {
-      alert('Chore already exists.');
-    }
-  };
+    // Function to remove a housemate
+    const removeHousemate = (housemateName) => {
+        setHousemates((prevHousemates) => prevHousemates.filter((housemate) => housemate.name !== housemateName));
+    };
 
-  // Function to assign a chore to a housemate
-  const assignChoreToHousemate = (choreName, housemateName) => {
-    setHousemates(prevHousemates =>
-      prevHousemates.map(housemate =>
-        housemate.name === housemateName
-          ? { ...housemate, chores: [...housemate.chores, choreName] }
-          : housemate
-      )
-    );
-    // Remove the chore from the list of unassigned chores
-    setChores(prevChores => prevChores.filter(chore => chore !== choreName));
-  };
 
-// Function to get the start and end of the current week in the format month/day/year
-const getCurrentWeek = () => {
-  const currentDate = new Date();
-  const startOfWeek = new Date(currentDate.setDate(currentDate.getDate() - currentDate.getDay()));
-  const endOfWeek = new Date(startOfWeek);
-  endOfWeek.setDate(endOfWeek.getDate() + 6);
+    // Function to add a new chore to the list
+    const addChoreToList = (choreName) => {
+        if (!chores.includes(choreName)) {
+            setChores(prevChores => [
+                ...prevChores,
+                choreName
+            ]);
+        } else {
+            alert('Chore already exists.');
+        }
+    };
 
-  const start = startOfWeek.toLocaleDateString('en-US'); // defaults to 'mm/dd/yyyy' for 'en-US'
-  const end = endOfWeek.toLocaleDateString('en-US');
+    // Function to remove a chore from the list
+    const removeChore = (choreName) => {
+        setChores(prevChores => prevChores.filter(chore => chore !== choreName));
+    };
 
-  return `${start} - ${end}`;
-};
+    // Function to assign a chore to a housemate
+    const assignChoreToHousemate = (choreName, housemateName) => {
+        setHousemates((prevHousemates) => prevHousemates.map((housemate) => {
+            if (housemate.name === housemateName) {
+                if (!housemate.chores.includes(choreName)) { // Add the chore to the housemate's chores
+                    const updatedChores = [
+                        ...housemate.chores,
+                        choreName
+                    ];
+                    const updatedHousemate = {
+                        ...housemate,
+                        chores: updatedChores
+                    };
+                    return updatedHousemate;
+                }
+            }
+            return housemate;
+        }));
 
-const handleDragStart = (e, chore) => {
-  e.dataTransfer.setData('text/plain', chore);
-};
+        // Remove the chore from the list of unassigned chores
+        setChores((prevChores) => prevChores.filter((chore) => chore !== choreName));
+    };
 
-const handleDrop = (e, housemateName) => {
-  e.preventDefault();
-  const chore = e.dataTransfer.getData('text/plain');
-  assignChoreToHousemate(chore, housemateName);
-};
+
+    // Function to delete a chore from a housemate and return it to the unassigned chores list
+    const deleteChoreFromHousemate = (choreName, housemateName) => {
+        setHousemates(prevHousemates => prevHousemates.map(housemate => {
+            if (housemate.name === housemateName && housemate.chores.includes(choreName)) {
+                const updatedChores = housemate.chores.filter(chore => chore !== choreName);
+                setChores(prevChores => {
+                    if (!prevChores.includes(choreName)) {
+                        return [
+                            ...prevChores,
+                            choreName
+                        ]; // Add the chore to the unassigned chores list only once
+                    }
+                    return prevChores;
+                });
+                return {
+                    ...housemate,
+                    chores: updatedChores
+                };
+            }
+            return housemate;
+        }));
+    };
+
+
+    // Function to get the start and end of the current week in the format month/day/year
+    const getCurrentWeek = () => {
+        const currentDate = new Date();
+        const startOfWeek = new Date(currentDate.setDate(currentDate.getDate() - currentDate.getDay()));
+        const endOfWeek = new Date(startOfWeek);
+        endOfWeek.setDate(endOfWeek.getDate() + 6);
+
+        const start = startOfWeek.toLocaleDateString('en-US'); // defaults to 'mm/dd/yyyy' for 'en-US'
+        const end = endOfWeek.toLocaleDateString('en-US');
+
+        return `${start} - ${end}`;
+    };
+
+    const handleDragStart = (e, chore) => {
+        e.dataTransfer.setData('text/plain', chore);
+    };
+
+    const handleDrop = (e, housemateName) => {
+        e.preventDefault();
+        const chore = e.dataTransfer.getData('text/plain');
+        assignChoreToHousemate(chore, housemateName);
+    };
+
+    const handleDragOver = (e) => {
+        e.preventDefault();
+    };
 
     const weekRange = getCurrentWeek();
-  
+
+    // Function to mark a chore as complete
+    const toggleChoreCompletion = (choreName, housemateName) => {
+        setHousemates((prevHousemates) => prevHousemates.map((housemate) => {
+            if (housemate.name === housemateName && housemate.chores.includes(choreName)) { // Increment the housemate's points
+                const updatedHousemate = {
+                    ...housemate,
+                    chores: housemate.chores.filter((chore) => chore !== choreName), // Remove the chore
+                    points: housemate.points + 1
+                };
+
+                // Remove the chore from teh housemate and add it back to the unassigned chore list
+                setChores(prevChores => {
+                    if (!prevChores.includes(choreName)) {
+                        return [
+                            ...prevChores,
+                            choreName
+                        ]
+                    }
+                    return prevChores
+                })
+
+                return updatedHousemate;
+            }
+            return housemate;
+        }));
+    };
+
+
     return (
-    <div className="app-container">
-      <h1>Chore Leaderboard</h1>
-      <div className="week-display">Week of: {weekRange}</div>
-      <HousemateForm addHousemate={addHousemate} />
-      <AddChoreForm addChoreToList={addChoreToList} />
-      <HousemateList
-  housemates={housemates}
-  //toggleChoreCompletion={toggleChoreCompletion} // Assuming this is still needed for other functionality
-  handleDrop={handleDrop}
-  handleDragOver={(e) => e.preventDefault()}
-/>
-      <div>
-        <h2>Unassigned Chores</h2>
-        <ul style={{listStyleType: 'none'}}>
-          {chores.map((chore, index) => (
-              <li
-              key={index}
-              draggable="true"
-              onDragStart={(e) => handleDragStart(e, chore)}
-            >
-              {chore}
-            </li>
-          ))}
-        </ul>
-      </div>
-    </div>
-  );
+        <div className="app-container">
+            <h1>Chore Leaderboard</h1>
+            <div className="week-display">Week of: {weekRange}</div>
+            <HousemateForm addHousemate={addHousemate}/>
+            <AddChoreForm addChoreToList={addChoreToList}/>
+            <HousemateList housemates={housemates}
+                handleDrop={handleDrop}
+                handleDragOver={handleDragOver}
+                deleteChoreFromHousemate={deleteChoreFromHousemate}
+                toggleChoreCompletion={toggleChoreCompletion}
+                removeHousemate={removeHousemate}/>
+
+            <div className="unassigned-chores">
+                <h2>Unassigned Chores</h2>
+                <ul> {
+                    chores.map((chore, index) => (
+                        <div key={index}
+                            className="chore-item"
+                            // Make sure this class is defined in your CSS
+                            draggable="true"
+                            onDragStart={
+                                (e) => handleDragStart(e, chore)
+                            }
+                            data-chore={chore}>
+                            {chore}
+                            <button className="delete-button"
+                                onClick={
+                                    () => removeChore(chore)
+                            }>
+                                Remove
+                            </button>
+                        </div>
+                    ))
+                } </ul>
+            </div>
+        </div>
+    );
 };
 
 export default App;
--- src/HousemateList.js
+++ src/HousemateList.js
@@ -1,29 +1,65 @@
 import React from 'react';
 import './HousemateList.css'; // Import the CSS for this component
 
-const HousemateList = ({ housemates, toggleChoreCompletion, handleDrop, handleDragOver }) => {
-  return (
-    <div className="housemate-list">
-      {housemates.map((housemate, index) => (
-        <div
-          key={index}
-          className="housemate-card"
-          onDrop={e => handleDrop(e, housemate.name)}
-          onDragOver={handleDragOver}
-        >
-          <h3 className="housemate-name">{housemate.name}</h3>
-          <ul>
-            {housemate.chores.map((chore, choreIndex) => (
-              <li key={choreIndex} className="chore-item">
-                {chore}
-                {/* Checkbox removed since chore completion is not part of the drag-and-drop functionality */}
-              </li>
-            ))}
-          </ul>
-        </div>
-      ))}
-    </div>
-  );
+const HousemateList = ({
+    housemates,
+    toggleChoreCompletion,
+    handleDrop,
+    handleDragOver,
+    deleteChoreFromHousemate,
+    removeHousemate
+}) => {
+    return (
+        <div className="housemate-list">
+            {
+            housemates.map((housemate, index) => (
+                <div key={index}
+                    className="housemate-card"
+                    onDrop={
+                        e => handleDrop(e, housemate.name)
+                    }
+                    onDragOver={handleDragOver}>
+                    <div className="housemate-header">
+                        <h3 className="housemate-name">
+                            {
+                            housemate.name
+                        }
+                            : {
+                            housemate.points
+                        } </h3>
+                        <button className="remove-button"
+                            onClick={
+                                () => removeHousemate(housemate.name)
+                        }>
+                            x
+                        </button>
+                    </div>
+                    <ul> {
+                        housemate.chores.map((chore, choreIndex) => (
+                            <li key={choreIndex}
+                                className="chore-item">
+                                {chore}
+                                <div className="chore-actions">
+                                    <button className="delete-button"
+                                        onClick={
+                                            () => deleteChoreFromHousemate(chore, housemate.name)
+                                    }>
+                                        Delete
+                                    </button>
+                                    <button className="complete-button"
+                                        onClick={
+                                            () => toggleChoreCompletion(chore, housemate.name)
+                                    }>
+                                        Complete
+                                    </button>
+                                </div>
+                            </li>
+                        ))
+                    } </ul>
+                </div>
+            ))
+        } </div>
+    );
 };
 
 export default HousemateList;
